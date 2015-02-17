453105782ae63324951c4eda80f27732026ffa36
tukiyo3 <tukiyo3@gmail.com>
2015-02-17 09:32:31 +0900

    作った

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # file2modday_renamer
 access_log.11.gz を access_log.2015-02-06.gz  にリネームする君
+
+* dryrun: ./rename.sh access_log --noop
+* execute: ./rename.sh access_log
---
+++ rename.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+set -eu
+
+error() {
+  echo "[quit] command error."
+  echo "example1: $0 access_log --noop"
+  echo "example2: $0 access_log"
+  exit 1
+}
+
+if [ $# -eq 0 ];then
+  error
+fi
+
+BASENAME=$1
+
+for i in ${BASENAME}.*.gz
+do
+  YMD=$(stat -c %y $i | cut -f 1 -d " ")
+  if [ $# -eq 2 ];then
+    if [ $2 = "--noop" ];then
+      echo mv $i ${BASENAME}.${YMD}
+    else
+      error
+    fi
+  else
+    mv $i ${BASENAME}.${YMD}
+  fi
+done
